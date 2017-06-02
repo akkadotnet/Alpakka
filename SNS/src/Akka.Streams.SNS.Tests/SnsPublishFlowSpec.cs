5849c77814826e6f54147a4aa874a780852462f5
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-06-02 11:22:17 +0100

    Add NSubstitute v2.3 to the tests project.

---
+++ SNS/src/Akka.Streams.SNS.Tests/SnsPublishFlowSpec.cs
@@ -0,0 +1,22 @@
+﻿using Xunit.Abstractions;
+
+namespace Akka.Streams.SignalR.Tests
+{
+    public class SnsPublishFlowSpec: Akka.TestKit.Xunit2.TestKit
+    {
+        private readonly ActorMaterializer materializer;
+
+        public SnsPublishFlowSpec(ITestOutputHelper output)
+            : base(output: output)
+        {
+            materializer = Sys.Materializer();
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            materializer.Dispose();
+        }
+        }
+}

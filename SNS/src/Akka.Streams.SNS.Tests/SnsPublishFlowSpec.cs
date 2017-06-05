9b1005a66665423ce64fe7378f1cafe37ba0e9fe
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-06-05 06:08:48 +0100

    Add a test checking that multiple messages are published to sns.

--- SNS/src/Akka.Streams.SNS.Tests/SnsPublishFlowSpec.cs
+++ SNS/src/Akka.Streams.SNS.Tests/SnsPublishFlowSpec.cs
@@ -10,6 +10,7 @@ using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace Akka.Streams.SignalR.Tests
 {
@@ -27,8 +28,7 @@ namespace Akka.Streams.SignalR.Tests
         public void ItShouldSendASingleMessageToSNS()
         {
             PublishRequest request = new PublishRequest("topic-arn", "sns-message");
-            PublishResponse response = new PublishResponse();
-            response.MessageId = "message-id";
+            var response = CreatePublishResponse("message-id");
             var snsService = Substitute.For<IAmazonSimpleNotificationService>();
             snsService.PublishAsync(request).Returns(response);
             var val = TestSource.SourceProbe<string>(this).Via(SnsPublisher.PublishToSNSFlow("topic-Arn", snsService)).ToMaterialized(Sink.Seq<PublishResponse>(), Keep.Both).Run(this.materializer);
@@ -37,7 +37,35 @@ namespace Akka.Streams.SignalR.Tests
             val.Item2.Result.ShouldBeEquivalentTo(ImmutableList.Create(response));
             snsService.Received(1).PublishAsync(request);
         }
-        
+
+        private static PublishResponse CreatePublishResponse(string responseMessage)
+        {
+            PublishResponse response = new PublishResponse();
+            response.MessageId = responseMessage;
+            return response;
+        }
+
+        [Fact]
+        public void ItShouldPublishMultipleMessagesToSNs()
+        {
+            var responseMessageStrings = Enumerable.Range(0, 3).Select(i => String.Format("message-id-{0}", i));
+            var expectedResponseMessages = ImmutableList.CreateRange(responseMessageStrings.Select(s =>CreatePublishResponse(s)));
+            var snsService = Substitute.For<IAmazonSimpleNotificationService>();
+            snsService.PublishAsync(Arg.Any<PublishRequest>())
+                .Returns(
+                    Task.FromResult(expectedResponseMessages.First()),
+                    expectedResponseMessages.Skip(1).Select(t => Task.FromResult(t)).ToArray());
+            var val = TestSource.SourceProbe<string>(this).Via(SnsPublisher.PublishToSNSFlow("topic-Arn", snsService)).ToMaterialized(Sink.Seq<PublishResponse>(), Keep.Both).Run(this.materializer);
+            foreach (var rms in responseMessageStrings)
+            {
+                val.Item1.SendNext(rms);
+            }
+            val.Item1.SendComplete();
+            var task =val.Item2.Wait(TimeSpan.FromSeconds(1)).Should().BeTrue();
+            val.Item2.Result.ShouldBeEquivalentTo(expectedResponseMessages);
+            snsService.ReceivedWithAnyArgs(3);
+        }
+            
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);

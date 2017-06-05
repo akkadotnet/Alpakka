5566b81cdcf95fbd7ddd8f6cc545f9bd36e3b021
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2017-06-05 16:32:11 +0100

    Port the code for the sns publish flow stage.

--- SNS/src/Akka.Streams.SNS/SNSPublishFlowStage.cs
+++ SNS/src/Akka.Streams.SNS/SNSPublishFlowStage.cs
@@ -2,6 +2,7 @@
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
 using System;
+using System.Threading.Tasks;
 
 namespace Akka.Streams.SNS
 {
@@ -9,6 +10,8 @@ namespace Akka.Streams.SNS
     {
         private string topicArn;
         private IAmazonSimpleNotificationService snsService;
+private readonly Inlet<string> inlet =new Inlet<string>("SnsPublishFlow.in");
+        private readonly Outlet<PublishResponse> outlet =new Outlet<PublishResponse>("SnsPublishFlow.out");
 
         public SNSPublishFlowStage(string topicArn, IAmazonSimpleNotificationService snsService)
         {
@@ -18,12 +21,79 @@ namespace Akka.Streams.SNS
         
         protected override GraphStageLogic CreateLogic(Attributes inheritedAttributes)
         {
-            throw new NotImplementedException();
+            return new logic(this, topicArn, snsService);
         }
+        #region logic
+
+        private sealed class logic : InAndOutGraphStageLogic
+        {
+            private bool isMessageInFlight = false;
+            private IAmazonSimpleNotificationService snsService;
+            private string topicArn;
+            private SNSPublishFlowStage stage;
+            private Action<PublishResponse> onSuccess;
+            private Action<Exception> onFailure;
+            
+            public logic(SNSPublishFlowStage stage, string topicArn, IAmazonSimpleNotificationService snsService)
+                : base(stage.Shape)
+            {
+                this.stage = stage;
+                this.topicArn = topicArn;
+                this.snsService = snsService;
+                this.onSuccess = GetAsyncCallback<PublishResponse>(HandleSuccess);
+                this.onFailure = GetAsyncCallback<Exception>(HandleFailure);
+            }
+
+            private void HandleFailure(Exception ex)
+            {
+                FailStage(ex);
+            }
+
+            private void HandleSuccess(PublishResponse response)
+            {
+                //add this when 1.3 comes out.
+                //log.debug("Published SNS message: {}", result.getMessageId)
+                isMessageInFlight = false;
+                if (!IsClosed(stage.outlet)) 
+                    Push(stage.outlet, response);
+            }
+
+            public override void OnPush()
+            {
+                isMessageInFlight = true;
+                var request = new PublishRequest();
+                request.TopicArn = topicArn;
+                request.Message = Grab(stage.inlet);
+                snsService.PublishAsync(request)
+                    .ContinueWith(
+                        task =>
+                            {
+                                if (task.IsFaulted || task.IsCanceled)
+                                    onFailure(task.Exception);
+                                else
+                                    onSuccess(task.Result);
+                            });
+            }
+            
+            public override void OnPull()
+            {
+                if (IsClosed(stage.inlet) && !isMessageInFlight) CompleteStage();
+                if (!HasBeenPulled(stage.inlet)) TryPull(stage.inlet);
+            }
+            
+            public override void OnUpstreamFinish()
+            {
+                if (!isMessageInFlight) CompleteStage();
+
+                SetHandler(stage.inlet, this);
+                SetHandler(stage.outlet, this);
+            }
+        }
+        #endregion
 
         public override FlowShape<string, PublishResponse> Shape
         {
-            get { throw new NotImplementedException(); }
+            get { return new FlowShape<string,PublishResponse>(inlet, outlet); }
         }
     }
 }

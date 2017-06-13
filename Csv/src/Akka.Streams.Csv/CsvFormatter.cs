8d5266b1321989251d28b15e1b965c454c47a8d4
Gregorius Soedharmo <arkatufus@yahoo.com>
2017-06-14 05:40:19 +0700

    Initial commit. Port CsvFormatting

---
+++ Csv/src/Akka.Streams.Csv/CsvFormatter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Akka.IO;
+using Akka.Streams.Csv.Dsl;
+
+namespace Akka.Streams.Csv
+{
+    /// <summary>
+    /// INTERNAL API
+    /// </summary>
+    public class CsvFormatter
+    {
+        private readonly char _delimiter;
+
+        private readonly char _quoteChar;
+        private readonly string _duplicatedQuote;
+
+        private readonly char _escapeChar;
+        private readonly string _duplicatedEscape;
+
+        private readonly char[] _quoteOrEscapechar;
+        private readonly char[] _needQuoteChars;
+
+        private readonly string _endOfLine;
+        private readonly ByteString _endOfLineBs;
+
+        private readonly CsvQuotingStyle _quotingStyle;
+        private readonly Encoding _encoding;
+
+        public string CharsetName => _encoding.EncodingName;
+
+        public CsvFormatter(char delimiter, char quoteChar, char escapeChar, string endOfLine, CsvQuotingStyle quotingStyle, Encoding encoding)
+        {
+            _delimiter = delimiter;
+
+            _quoteChar = quoteChar;
+            _duplicatedQuote = new string(quoteChar, 2);
+
+            _escapeChar = escapeChar;
+            _duplicatedEscape = new string(escapeChar, 2);
+
+            _quoteOrEscapechar = new[] {quoteChar, escapeChar};
+            _needQuoteChars = new[] {'\r', '\n', delimiter};
+
+            _endOfLine = endOfLine;
+            _endOfLineBs = ByteString.FromString(_endOfLine);
+
+            _quotingStyle = quotingStyle;
+            _encoding = encoding;
+        }
+
+        
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public ByteString ToCsv(IEnumerable fields)
+        {
+            if (fields == null)
+            {
+                return _endOfLineBs;
+            }
+
+            if (fields.GetEnumerator().MoveNext())
+            {
+                if (_encoding == null)
+                {
+                    return ByteString.FromString(NonEmptyToCsv(fields));
+                }
+                return ByteString.FromString(NonEmptyToCsv(fields), _encoding);
+            }
+
+            return _endOfLineBs;
+        }
+
+        private string NonEmptyToCsv(IEnumerable fields)
+        {
+            var builder = new StringBuilder();
+
+            void SplitAndDuplicateQuotesAndEscapes(string field, int splitAt)
+            {
+                var lastIndex = 0;
+                var index = splitAt;
+                while (index > -1)
+                {
+                    builder.Append(field.Substring(lastIndex, index - lastIndex));
+                    builder.Append(field[index] == _quoteChar ? _duplicatedQuote : _duplicatedEscape);
+                    lastIndex = index + 1;
+                    index = field.IndexOfAny(_quoteOrEscapechar, lastIndex);
+                }
+                if (lastIndex < field.Length)
+                {
+                    builder.Append(field.Substring(lastIndex));
+                }
+            }
+
+            void Append(string field)
+            {
+                int splitAt;
+                var quoteIt = RequiresQuoteOrSplit(field, out splitAt);
+                if (quoteIt)
+                {
+                    builder.Append(_quoteChar);
+                    if (splitAt != -1)
+                        SplitAndDuplicateQuotesAndEscapes(field, splitAt);
+                    else
+                        builder.Append(field);
+                    builder.Append(_quoteChar);
+                }
+                else
+                {
+                    builder.Append(field);
+                }
+            }
+
+            var iterator = fields.GetEnumerator();
+            var hasNext = iterator.MoveNext();
+            var value = iterator.Current;
+            while (hasNext)
+            {
+                if (value != null)
+                {
+                    Append(value.ToString());
+                }
+                hasNext = iterator.MoveNext();
+                if (hasNext)
+                {
+                    builder.Append(_delimiter);
+                }
+            }
+            builder.Append(_endOfLine);
+            return builder.ToString();
+        }
+
+        private bool RequiresQuoteOrSplit(string field, out int splitIndex)
+        {
+            splitIndex = field.IndexOfAny(_quoteOrEscapechar);
+            return CsvQuotingStyle.Always == _quotingStyle || splitIndex != -1 || field.IndexOfAny(_needQuoteChars) != -1;
+        }
+    }
+}
---
+++ Csv/src/Akka.Streams.Csv/Dsl/CsvFormatting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+using Akka.Streams.Dsl;
+
+namespace Akka.Streams.Csv.Dsl
+{
+    /// <summary>
+    /// Provides CSV formatting flows that convert a sequence of String into their CSV representation
+    /// in <see cref="Akka.IO.ByteString"/>.
+    /// </summary>
+    public class CsvFormatting
+    {
+        private const char _backslash = '\\';
+        private const char _comma = ',';
+        private const char _semiColon = ';';
+        private const char _colon = ':';
+        private const char _tab = '\t';
+        private const char _doubleQuote = '"';
+
+        /// <summary>
+        /// Create a Flow for converting IImmutableList&lt;string&gt; to ByteString.
+        /// </summary>
+        /// <param name="delimiter"></param>
+        /// <param name="quoteChar"></param>
+        /// <param name="escapeChar"></param>
+        /// <param name="endOfLine"></param>
+        /// <param name="quotingStyle"></param>
+        /// <param name="encoding"></param>
+        /// <param name="byteOrderMark"></param>
+        public Flow<IImmutableList<string>, ByteString, NotUsed> Format(
+            char delimiter = _comma, 
+            char quoteChar = _doubleQuote, 
+            char escapeChar = _backslash, 
+            string endOfLine = "\r\n", 
+            CsvQuotingStyle quotingStyle = CsvQuotingStyle.Required, 
+            Encoding encoding = null,
+            ByteString byteOrderMark = null)
+        {
+            var formatter = new CsvFormatter(delimiter, quoteChar, escapeChar, endOfLine, quotingStyle, encoding);
+
+            if (byteOrderMark == null)
+            {
+                return Flow.FromFunction<IImmutableList<string>, ByteString>(list => formatter.ToCsv(list))
+                    .Named("CsvFormatting");
+            }
+
+            return Flow.FromFunction<IImmutableList<string>, ByteString>(list => formatter.ToCsv(list))
+                .Prepend(Source.Single(byteOrderMark))
+                .Named("CsvFormatting");
+        }
+    }
+}

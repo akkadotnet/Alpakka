6615bcb0277da706413ddbd423747a1dc332462e
Gregorius Soedharmo <arkatufus@yahoo.com>
2017-06-16 00:37:04 +0700

    Port CsvToMap to CsvToDictionary

---
+++ Csv/src/Akka.Streams.Csv/CsvToDictionaryStage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+using Akka.Streams.Stage;
+
+namespace Akka.Streams.Csv
+{
+    /// <summary>
+    /// Internal API: Converts incoming ImmutableList&lt;ByteString&gt; to Dictionary&lt;string, ByteString&gt;
+    /// </summary>
+    internal class CsvToDictionaryStage:GraphStage<FlowShape<ImmutableList<ByteString>, Dictionary<string, ByteString>>>
+    {
+        #region Logic
+        private sealed class Logic : InAndOutGraphStageLogic
+        {
+            private readonly CsvToDictionaryStage _stage;
+            private ImmutableList<string> _headers;
+            private readonly Encoding _encoding;
+
+            public Logic(CsvToDictionaryStage stage, ImmutableList<string> columnNames, Encoding encoding) : base(stage.Shape)
+            {
+                _stage = stage;
+                _headers = columnNames;
+                _encoding = encoding;
+
+                SetHandler(stage.In, this);
+                SetHandler(stage.Out, this);
+            }
+
+            public override void OnPush()
+            {
+                var elements = Grab(_stage.In);
+                if (_headers != null)
+                {
+                    var map = new Dictionary<string, ByteString>();
+                    var len = Math.Min(_headers.Count, elements.Count);
+                    for(int i = 0; i < len; ++i)
+                    {
+                        map.Add(_headers[i], elements[i]);
+                    }
+                    Push(_stage.Out, map);
+                }
+                else
+                {
+                    var headers = new List<string>();
+                    foreach (var elem in elements)
+                    {
+                        headers.Add(elem.DecodeString(_encoding));
+                    }
+                    _headers = headers.ToImmutableList();
+                    Pull(_stage.In);
+                }
+            }
+
+            public override void OnPull()
+            {
+                Pull(_stage.In);
+            }
+
+        }
+        #endregion
+
+        private readonly ImmutableList<string> _columnNames;
+        private readonly Encoding _encoding;
+
+        /// <summary>
+        /// Internal API: Converts incoming ImmutableList&lt;ByteString&gt; to Dictionary&lt;string, ByteString&gt;
+        /// </summary>
+        /// <param name="columnNames">If given, these names are used as map keys; if not first stream element is used</param>
+        /// <param name="encoding">Character encoding used to convert header line ByteString to String</param>
+        public CsvToDictionaryStage(ImmutableList<string> columnNames, Encoding encoding)
+        {
+            _columnNames = columnNames;
+            _encoding = encoding;
+
+            Shape = new FlowShape<ImmutableList<ByteString>, Dictionary<string, ByteString>>(In, Out);
+        }
+
+        protected override Attributes InitialAttributes { get; } = Attributes.CreateName("CsvToMap");
+
+        internal Inlet<ImmutableList<ByteString>> In { get; } = new Inlet<ImmutableList<ByteString>>("CsvToMap.in");
+        internal Outlet<Dictionary<string, ByteString>> Out { get; } = new Outlet<Dictionary<string, ByteString>>("CsvToMap.out");
+
+        public override FlowShape<ImmutableList<ByteString>, Dictionary<string, ByteString>> Shape { get; }
+
+        protected override GraphStageLogic CreateLogic(Attributes inheritedAttributes)
+            => new Logic(this, _columnNames, _encoding);
+
+        public override string ToString() => nameof(CsvParsingStage);
+    }
+}
---
+++ Csv/src/Akka.Streams.Csv/Dsl/CsvToDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+using Akka.Streams.Dsl;
+
+namespace Akka.Streams.Csv.Dsl
+{
+    public static class CsvToDictionary
+    {
+        /// <summary>
+        /// A flow translating incoming ImmutableList&lt;ByteString&gt; to Dictionary&lt;string, ByteString&gt; using the streams first element's values as keys.
+        /// </summary>
+        /// <param name="encoding">the encoding to decode <see cref="ByteString"/> to <see cref="string"/>, defaults to <see cref="Encoding.UTF8"/></param>
+        /// <returns></returns>
+        public static Flow<ImmutableList<ByteString>, Dictionary<string, ByteString>, NotUsed> ToMap(Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+
+            return Flow.FromGraph(new CsvToDictionaryStage(null, encoding));
+        }
+
+        /// <summary>
+        /// A flow translating incoming ImmutableList&lt;ByteString&gt; to Dictionary&lt;string, ByteString&gt; using the given headers as keys.
+        /// </summary>
+        /// <param name="headers">column names to be used as dictionary keys</param>
+        /// <returns></returns>
+        public static Flow<ImmutableList<ByteString>, Dictionary<string, ByteString>, NotUsed> WithHeaders(ImmutableList<string> headers)
+        {
+            if(headers == null)
+                throw new ArgumentException($"{nameof(headers)} was null.", nameof(headers));
+
+            return Flow.FromGraph(new CsvToDictionaryStage(headers, Encoding.UTF8));
+        }
+    }
+}

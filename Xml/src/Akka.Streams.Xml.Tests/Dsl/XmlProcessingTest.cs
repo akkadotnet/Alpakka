a83e5f4699fc55c438a66340acf97f11a09a47d2
Gregorius Soedharmo <arkatufus@yahoo.com>
2017-08-08 03:05:45 +0700

    Port Alpakka Xml (#13)
    * Add Xml.cs and XmlParsing.cs
    
    * Attempt to make .net XmlReader to work nicely inside Stream Logic
    
    * Semi working XmlReader. Reader still fails because I could not emulate EVENT_INCOMPLETE in .net
    
    * Make XmlReader parse asynchronously
    
    * Add XmlSubsliceTest port, fix coalesce test to test for CDATA tag only, minor XmlParsing documentation edit.
    
    * Change all async and threading mess with cleaner sychronous solution.
    
    * Add multi document support. Add readme.md file, change akka version in test project to match with the main project.
    
    * Refactor code to C# 7 conventions

---
+++ Xml/src/Akka.Streams.Xml.Tests/Dsl/XmlProcessingTest.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+using Akka.Streams.Dsl;
+using Akka.Streams.Xml.Dsl;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Streams.Xml.Tests.Dsl
+{
+    public class XmlProcessingTest : Akka.TestKit.Xunit.TestKit
+    {
+        private readonly ActorMaterializer _materializer;
+        private readonly Sink<string, Task<IImmutableList<IParseEvent>>> _parser;
+
+        public XmlProcessingTest(ITestOutputHelper output) : base(output: output)
+        {
+            _materializer = Sys.Materializer();
+
+            _parser = Flow.Create<string>()
+                .Select(ByteString.FromString)
+                .Via(XmlParsing.Parser())
+                .ToMaterialized(Sink.Seq<IParseEvent>(), Keep.Right);
+        }
+
+        [Fact]
+        public void XmlParser_must_properly_parse_simple_XML()
+        {
+            var doc = "<doc><elem>elem1</elem><elem>elem2</elem></doc>";
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+            
+            fut.Wait(TimeSpan.FromSeconds(3));
+            var result = fut.Result;
+
+            result[0].Should().BeOfType<StartDocument>();
+            (result[1] as StartElement).ShouldBeEquivalentTo(new StartElement("doc", new Dictionary<string, string>()));
+            (result[2] as StartElement).ShouldBeEquivalentTo(new StartElement("elem", new Dictionary<string, string>()));
+            (result[3] as Characters).ShouldBeEquivalentTo(new Characters("elem1"));
+            (result[4] as EndElement).ShouldBeEquivalentTo(new EndElement("elem"));
+            (result[5] as StartElement).ShouldBeEquivalentTo(new StartElement("elem", new Dictionary<string, string>()));
+            (result[6] as Characters).ShouldBeEquivalentTo(new Characters("elem2"));
+            (result[7] as EndElement).ShouldBeEquivalentTo(new EndElement("elem"));
+            (result[8] as EndElement).ShouldBeEquivalentTo(new EndElement("doc"));
+            result[9].Should().BeOfType<EndDocument>();
+        }
+
+        [Fact]
+        public void XmlParser_must_properly_parse_multiple_XML_in_a_single_stream()
+        {
+            var doc = "<doc><elem>elem1</elem></doc>";
+            var fut = Source
+                .Single(doc)
+                .Concat(Source.Single(doc))
+                .RunWith(_parser, _materializer);
+
+            fut.Wait(TimeSpan.FromSeconds(3));
+            var result = fut.Result;
+
+            result[0].Should().BeOfType<StartDocument>();
+            (result[1] as StartElement).ShouldBeEquivalentTo(new StartElement("doc", new Dictionary<string, string>()));
+            (result[2] as StartElement).ShouldBeEquivalentTo(new StartElement("elem", new Dictionary<string, string>()));
+            (result[3] as Characters).ShouldBeEquivalentTo(new Characters("elem1"));
+            (result[4] as EndElement).ShouldBeEquivalentTo(new EndElement("elem"));
+            (result[5] as EndElement).ShouldBeEquivalentTo(new EndElement("doc"));
+            result[6].Should().BeOfType<EndDocument>();
+            result[7].Should().BeOfType<StartDocument>();
+            (result[8] as StartElement).ShouldBeEquivalentTo(new StartElement("doc", new Dictionary<string, string>()));
+            (result[9] as StartElement).ShouldBeEquivalentTo(new StartElement("elem", new Dictionary<string, string>()));
+            (result[10] as Characters).ShouldBeEquivalentTo(new Characters("elem1"));
+            (result[11] as EndElement).ShouldBeEquivalentTo(new EndElement("elem"));
+            (result[12] as EndElement).ShouldBeEquivalentTo(new EndElement("doc"));
+            result[13].Should().BeOfType<EndDocument>();
+        }
+
+        [Fact]
+        public void XmlParser_must_properly_process_a_comment()
+        {
+            var doc = "<doc><!--comment--></doc>";
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+
+            fut.Wait(TimeSpan.FromSeconds(3));
+            var result = fut.Result;
+
+            result[0].Should().BeOfType<StartDocument>();
+            (result[1] as StartElement).ShouldBeEquivalentTo(new StartElement("doc", new Dictionary<string, string>()));
+            (result[2] as Comment).ShouldBeEquivalentTo(new Comment("comment"));
+            (result[3] as EndElement).ShouldBeEquivalentTo(new EndElement("doc"));
+            result[4].Should().BeOfType<EndDocument>();
+        }
+
+        [Fact]
+        public void XmlParser_must_properly_process_parse_instructions()
+        {
+            var doc = "<?target content?><doc></doc>";
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+
+            fut.Wait(TimeSpan.FromSeconds(3));
+            var result = fut.Result;
+
+            result[0].Should().BeOfType<StartDocument>();
+            (result[1] as ProcessingInstruction).ShouldBeEquivalentTo(new ProcessingInstruction("target", "content"));
+            (result[2] as StartElement).ShouldBeEquivalentTo(new StartElement("doc", new Dictionary<string, string>()));
+            (result[3] as EndElement).ShouldBeEquivalentTo(new EndElement("doc"));
+            result[4].Should().BeOfType<EndDocument>();
+        }
+
+        [Fact]
+        public void XmlParser_must_properly_process_attributes()
+        {
+            var doc = "<doc good=\"yes\"><elem nice=\"yes\" very=\"true\">elem1</elem></doc>";
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+
+            fut.Wait(TimeSpan.FromSeconds(3));
+            var result = fut.Result;
+
+            result[0].Should().BeOfType<StartDocument>();
+            (result[1] as StartElement).ShouldBeEquivalentTo(new StartElement("doc", new Dictionary<string, string> {{"good", "yes"}}));
+            (result[2] as StartElement).ShouldBeEquivalentTo(new StartElement("elem", new Dictionary<string, string> {{"nice", "yes"}, {"very", "true"}}));
+            (result[3] as Characters).ShouldBeEquivalentTo(new Characters("elem1"));
+            (result[4] as EndElement).ShouldBeEquivalentTo(new EndElement("elem"));
+            (result[5] as EndElement).ShouldBeEquivalentTo(new EndElement("doc"));
+            result[6].Should().BeOfType<EndDocument>();
+        }
+
+        [Fact]
+        public void XmlParser_must_properly_process_CDATA_blocks()
+        {
+            var doc = "<doc><![CDATA[<not>even</valid>]]></doc>";
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+
+            fut.Wait(TimeSpan.FromSeconds(3));
+            var result = fut.Result;
+
+            result[0].Should().BeOfType<StartDocument>();
+            (result[1] as StartElement).ShouldBeEquivalentTo(new StartElement("doc", new Dictionary<string, string>()));
+            (result[2] as CData).ShouldBeEquivalentTo(new CData("<not>even</valid>"));
+            (result[3] as EndElement).ShouldBeEquivalentTo(new EndElement("doc"));
+            result[4].Should().BeOfType<EndDocument>();
+        }
+
+        // I'm not sure what the original very convoluted code does
+        [Fact]
+        public void XmlParser_must_properly_parse_large_XML()
+        {
+            var elements = Enumerable.Range(0, 10).Select(i => i.ToString()).ToImmutableArray();
+
+            var documentStream = Source
+                .Single("<doc>")
+                .Concat(Source.FromEnumerator(elements.Select(s => $"<elem>{s}</elem>").GetEnumerator))
+                .Concat(Source.Single("</doc>"));
+
+            var fut = documentStream
+                .Select(ByteString.FromString)
+                .Via(XmlParsing.Parser())
+                .Collect(evt => evt is Characters ? evt : null)
+                .Collect(evt => ((Characters) evt).Text)
+                .RunWith(Sink.Seq<string>(), _materializer);
+
+            fut.Wait(TimeSpan.FromSeconds(3));
+            fut.Result.ShouldAllBeEquivalentTo(elements);
+        }
+
+        [Fact]
+        public void XmlParser_must_properly_parse_chunks_bigger_than_its_buffer_size()
+        {
+            var documentStream = Source
+                .Single("<doc>")
+                .Concat(Source.Single(
+@"  <elem>
+    <item>i1</item>
+    <item>i2</item>
+    <item>i3</item>
+    <item>i1</item>
+    <item>i2</item>
+    <item>i3</item>
+    <item>i1</item>
+    <item>i2</item>
+    <item>i3</item>
+    <item>i1</item>
+    <item>i2</item>
+    <item>i3</item>
+  </elem>"
+                ))
+                .Concat(Source.Single("</doc>"));
+
+            var fut = documentStream
+                .Select(ByteString.FromString)
+                .Via(XmlParsing.Parser(bufferSize:64))
+                .RunWith(Sink.Seq<IParseEvent>(), _materializer);
+
+            fut.Invoking(f => f.Wait(TimeSpan.FromSeconds(3))).ShouldNotThrow();
+        }
+    }
+}
---
+++ Xml/src/Akka.Streams.Xml.Tests/Dsl/XmlSubsliceTest.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+using Akka.Streams.Dsl;
+using Akka.Streams.Xml.Dsl;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Streams.Xml.Tests.Dsl
+{
+    public class XmlSubsliceTest : Akka.TestKit.Xunit.TestKit
+    {
+        private readonly ActorMaterializer _materializer;
+        private readonly Sink<string, Task<IImmutableList<IParseEvent>>> _parser;
+
+        public XmlSubsliceTest(ITestOutputHelper output) : base(output: output)
+        {
+            _materializer = Sys.Materializer();
+
+            _parser = Flow.Create<string>()
+                .Select(ByteString.FromString)
+                .Via(XmlParsing.Parser())
+                .Via(XmlParsing.Subslice(new []{"doc", "elem", "item"}.ToImmutableArray()))
+                .ToMaterialized(Sink.Seq<IParseEvent>(), Keep.Right);
+        }
+
+        [Fact]
+        public void XmlSubslice_support_must_properly_extract_subslices_of_events()
+        {
+            var doc =
+@"<doc>
+  <elem>
+    <item>i1</item>
+    <item>i2</item>
+    <item>i3</item>
+  </elem>
+</doc>";
+
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+            fut.Wait(TimeSpan.FromSeconds(3));
+
+            var result = fut.Result;
+            ((Characters) result[0]).ShouldBeEquivalentTo(new Characters("i1"));
+            ((Characters) result[1]).ShouldBeEquivalentTo(new Characters("i2"));
+            ((Characters) result[2]).ShouldBeEquivalentTo(new Characters("i3"));
+        }
+
+        [Fact]
+        public void XmlSubslice_support_must_properly_extract_subslices_of_nested_events()
+        {
+            var doc =
+@"<doc>
+  <elem>
+    <item>i1</item>
+    <item><sub>i2</sub></item>
+    <item>i3</item>
+  </elem>
+</doc>";
+
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+            fut.Wait(TimeSpan.FromSeconds(3));
+
+            var result = fut.Result;
+            ((Characters)result[0]).ShouldBeEquivalentTo(new Characters("i1"));
+            ((StartElement) result[1]).ShouldBeEquivalentTo(new StartElement("sub", new Dictionary<string, string>()));
+            ((Characters)result[2]).ShouldBeEquivalentTo(new Characters("i2"));
+            ((EndElement)result[3]).ShouldBeEquivalentTo(new EndElement("sub"));
+            ((Characters)result[4]).ShouldBeEquivalentTo(new Characters("i3"));
+        }
+
+        [Fact]
+        public void XmlSubslice_support_must_properly_ignore_matches_not_deep_enough()
+        {
+            var doc =
+@"<doc>
+  <elem>
+    I am lonely here :(
+  </elem>
+</doc>";
+
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+            fut.Wait(TimeSpan.FromSeconds(3));
+
+            fut.Result.Count.Should().Be(0);
+        }
+
+
+        [Fact]
+        public void XmlSubslice_support_must_properly_ignore_partial_matches()
+        {
+            var doc =
+@"<doc>
+  <elem>
+    <notanitem>ignore me</notanitem>
+    <notanitem>ignore me</notanitem>
+    <foo>ignore me</foo>
+  </elem>
+</doc>";
+
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+            fut.Wait(TimeSpan.FromSeconds(3));
+
+            fut.Result.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void XmlSubslice_support_must_properly_filter_from_the_combination_of_the_above()
+        {
+            var doc =
+@"<doc>
+  <elem>
+    <notanitem>ignore me</notanitem>
+    <notanitem>ignore me</notanitem>
+    <foo>ignore me</foo>
+    <item>i1</item>
+    <item><sub>i2</sub></item>
+    <item>i3</item>
+  </elem>
+  <elem>
+    not me please
+  </elem>
+  <elem><item>i4</item></elem>
+</doc>";
+
+            var fut = Source.Single(doc).RunWith(_parser, _materializer);
+            fut.Wait(TimeSpan.FromSeconds(3));
+
+            var result = fut.Result;
+            ((Characters)result[0]).ShouldBeEquivalentTo(new Characters("i1"));
+            ((StartElement)result[1]).ShouldBeEquivalentTo(new StartElement("sub", new Dictionary<string, string>()));
+            ((Characters)result[2]).ShouldBeEquivalentTo(new Characters("i2"));
+            ((EndElement)result[3]).ShouldBeEquivalentTo(new EndElement("sub"));
+            ((Characters)result[4]).ShouldBeEquivalentTo(new Characters("i3"));
+            ((Characters)result[5]).ShouldBeEquivalentTo(new Characters("i4"));
+
+        }
+    }
+}

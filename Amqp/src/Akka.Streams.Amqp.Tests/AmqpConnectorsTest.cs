4c9c238fb381c06c85660b3e6a416e1cdf0efa26
Vasily Kirichenko <vaskir@gmail.com>
2018-01-16 21:15:15 +0300

    add a test

--- Amqp/src/Akka.Streams.Amqp.Tests/AmqpConnectorsTest.cs
+++ Amqp/src/Akka.Streams.Amqp.Tests/AmqpConnectorsTest.cs
@@ -1,33 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using Akka.Actor;
 using Akka.IO;
 using Akka.Streams.Amqp.Dsl;
 using Akka.Streams.Dsl;
+using Akka.Streams.TestKit;
 using Xunit;
 
 namespace Akka.Streams.Amqp.Tests
 {
-    public class AmqpConnectorsTest : IDisposable
+    public class AmqpConnectorsTest : Akka.TestKit.Xunit2.TestKit
     {
-        private readonly ActorSystem _system;
-        private readonly IMaterializer _materializer;
+        private readonly AmqpConnectionDetails _connectionSettings;
+        private readonly ActorMaterializer _mat;
 
         public AmqpConnectorsTest()
         {
-            _system = ActorSystem.Create(GetType().Name);
-            _materializer = _system.Materializer();
-        }
-
-        [Fact]
-        public void PublishAndConsume()
-        {
-            var connectionSettings =
+            _mat = ActorMaterializer.Create(Sys);
+            _connectionSettings = 
                 AmqpConnectionDetails.Create("localhost", 5672)
                     .WithAutomaticRecoveryEnabled(true)
                     .WithNetworkRecoveryInterval(TimeSpan.FromSeconds(1));
+        }
 
+        [Fact]
+        public void Publish_and_consume_elements_through_a_simple_queue_again_in_the_same_process()
+        {
             var exchange = ExchangeDeclaration.Create("logs", "topic");
 
             //queue declaration
@@ -36,32 +34,64 @@ namespace Akka.Streams.Amqp.Tests
 
             //create sink
             var amqpSink = AmqpSink.CreateSimple(
-                AmqpSinkSettings.Create(connectionSettings)
+                AmqpSinkSettings.Create(_connectionSettings)
                     .WithRoutingKey(queueName)
                     .WithDeclarations(queueDeclaration));
 
             //create source
-            const int bufferSize = 10;
             var amqpSource = AmqpSource.AtMostOnceSource(
-                NamedQueueSourceSettings.Create(DefaultAmqpConnection.Instance, queueName)
-                    .WithDeclarations(queueDeclaration),
-                bufferSize);
+                NamedQueueSourceSettings.Create(DefaultAmqpConnection.Instance, queueName).WithDeclarations(queueDeclaration),
+                bufferSize: 10);
 
             //run sink
-            var input = new List<string> {"one", "two", "three", "four", "five"};
-            Source.From(input).Select(ByteString.FromString).RunWith(amqpSink, _materializer).Wait();
+            var input = new [] {"one", "two", "three", "four", "five"};
+            Source.From(input).Select(ByteString.FromString).RunWith(amqpSink, _mat).Wait();
 
             //run source
             var result =
                 amqpSource.Select(m => m.Bytes.ToString(Encoding.UTF8))
-                    .Take(input.Count)
-                    .RunWith(Sink.Seq<string>(), _materializer);
+                    .Take(input.Length)
+                    .RunWith(Sink.Seq<string>(), _mat);
 
             result.Wait(TimeSpan.FromSeconds(3));
             Assert.Equal(input, result.Result);
         }
+        
+        [Fact]
+        public void Publish_via_RPC_and_then_consume_through_a_simple_queue_again_in_the_same_process()
+        {
+            var queueName = "amqp-conn-it-spec-rpc-queue-" + Environment.TickCount;
+            var queueDeclaration = QueueDeclaration.Create(queueName);
 
+            //#create-rpc-flow
+            var amqpRpcFlow = AmqpRpcFlow.CreateSimple(
+                AmqpSinkSettings.Create(_connectionSettings).WithRoutingKey(queueName).WithDeclarations(queueDeclaration));
+            
+            //#create-rpc-flow
+            var amqpSource = AmqpSource.AtMostOnceSource(NamedQueueSourceSettings.Create(_connectionSettings, queueName), bufferSize: 1);
 
-        public void Dispose() => _system.Terminate().Wait();
+            var input = new[] {"one", "two", "three", "four", "five"};
+            
+            //#run-rpc-flow
+            var t = Source.From(input)
+                .Select(ByteString.FromString)
+                .ViaMaterialized(amqpRpcFlow, Keep.Right)
+                .ToMaterialized(this.SinkProbe<ByteString>(), Keep.Both)
+                .Run(_mat);
+            
+            var rpcQueueF = t.Item1;
+            var probe = t.Item2;
+            
+            //#run-rpc-flow
+            rpcQueueF.Wait();
+
+            var amqpSink = AmqpSink.Create(AmqpSinkSettings.Create(_connectionSettings));
+
+            amqpSource
+                .Select(b => OutgoingMessage.Create(b.Bytes.Concat(ByteString.FromString("a")), false, false, b.Properties))
+                .RunWith(amqpSink, _mat);
+
+            probe.Request(5).ExpectNextUnorderedN(input.Select(s => ByteString.FromString(s + "a"))).ExpectComplete();
+        }
     }
 }

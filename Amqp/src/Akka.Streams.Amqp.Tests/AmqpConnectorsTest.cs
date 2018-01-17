5691b1f3d065e8f5621c7c41a9aefe8cfb9e705d
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2018-01-17 16:14:59 +0300

    all tests ported and pass

--- Amqp/src/Akka.Streams.Amqp.Tests/AmqpConnectorsTest.cs
+++ Amqp/src/Akka.Streams.Amqp.Tests/AmqpConnectorsTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
 using Akka.IO;
 using Akka.Streams.Amqp.Dsl;
 using Akka.Streams.Dsl;
@@ -179,6 +182,292 @@ namespace Akka.Streams.Amqp.Tests
             {
                 ex.InnerException?.Message.Should().Be("Reply-to header was not set");
             }
+        }
+        
+        [Fact]
+        public void Not_fail_on_a_fast_producer_and_a_slow_consumer()
+        {
+            var queueName = "amqp-conn-it-spec-simple-queue-2-" + Environment.TickCount;
+            var queueDeclaration = QueueDeclaration.Create(queueName);
+            
+            var amqpSource = AmqpSource.AtMostOnceSource(
+                NamedQueueSourceSettings.Create(_connectionSettings, queueName).WithDeclarations(queueDeclaration), bufferSize: 2);
+
+            var amqpSink = AmqpSink.CreateSimple(AmqpSinkSettings.Create(_connectionSettings).WithRoutingKey(queueName).WithDeclarations(queueDeclaration));
+            var publisher = this.CreatePublisherProbe<ByteString>();
+            var subscriber = this.CreateSubscriberProbe<IncomingMessage>();
+            amqpSink.AddAttributes(Attributes.CreateInputBuffer(1, 1)).RunWith(Source.FromPublisher(publisher), _mat);
+            amqpSource.AddAttributes(Attributes.CreateInputBuffer(1, 1)).RunWith(Sink.FromSubscriber(subscriber), _mat);
+
+            // note that this essentially is testing rabbitmq just as much as it tests our sink and source
+            publisher.EnsureSubscription();
+            subscriber.EnsureSubscription();
+
+            publisher.ExpectRequest().Should().Be(1);
+            publisher.SendNext(ByteString.FromString("one"));
+
+            publisher.ExpectRequest();
+            publisher.SendNext(ByteString.FromString("two"));
+
+            publisher.ExpectRequest();
+            publisher.SendNext(ByteString.FromString("three"));
+
+            publisher.ExpectRequest();
+            publisher.SendNext(ByteString.FromString("four"));
+
+            publisher.ExpectRequest();
+            publisher.SendNext(ByteString.FromString("five"));
+
+            subscriber.Request(4);
+            subscriber.ExpectNext().Bytes.ToString().Should().Be("one");
+            subscriber.ExpectNext().Bytes.ToString().Should().Be("two");
+            subscriber.ExpectNext().Bytes.ToString().Should().Be("three");
+            subscriber.ExpectNext().Bytes.ToString().Should().Be("four");
+
+            subscriber.Request(1);
+            subscriber.ExpectNext().Bytes.ToString().Should().Be("five");
+
+            subscriber.Cancel();
+            publisher.SendComplete();
+        }
+        
+        [Fact]
+        public void Pub_sub_from_one_source_with_multiple_sinks() 
+        {
+            // with pubsub we arrange one exchange which the sink writes to and then one queue for each source which subscribes to the
+            // exchange - all this described by the declarations
+
+            //#exchange-declaration
+            var exchangeName = "amqp-conn-it-spec-pub-sub-" + Environment.TickCount;
+            var exchangeDeclaration = ExchangeDeclaration.Create(exchangeName, "fanout");
+
+            //#create-exchange-sink
+            var amqpSink = AmqpSink.CreateSimple(AmqpSinkSettings.Create(_connectionSettings).WithExchange(exchangeName).WithDeclarations(exchangeDeclaration));
+
+            //#create-exchange-source
+            const int fanoutSize = 4;
+
+            var mergedSources =
+                Enumerable.Range(0, fanoutSize)
+                    .Aggregate(Source.Empty<(int, string)>(), (source, fanoutBranch) =>
+                        source.Merge(
+                            AmqpSource.AtMostOnceSource(
+                                    TemporaryQueueSourceSettings.Create(_connectionSettings, exchangeName).WithDeclarations(exchangeDeclaration), bufferSize: 1)
+                                .Select(msg => (branch: fanoutBranch, message: msg.Bytes.ToString())))
+                    );
+
+            var completion = new TaskCompletionSource<Done>();
+
+            mergedSources.RunWith(Sink.Aggregate<(int branch, string msg), ImmutableHashSet<int>>(ImmutableHashSet.Create<int>(), (seen, t) =>
+            {
+                if (seen.Count == fanoutSize) completion.SetResult(Done.Instance);
+                return seen.Add(t.branch);
+            }), _mat);
+
+            Sys.Scheduler.Advanced.ScheduleOnce(
+                TimeSpan.FromSeconds(5), 
+                () => completion.TrySetException(new Exception("Did not get at least one element from every fanout branch")));
+
+            Source.Repeat("stuff").Select(ByteString.FromString).RunWith(amqpSink, _mat);
+            completion.Task.Result.Should().Be(Done.Instance);
+        }
+        
+        [Fact]
+        public void Publish_and_consume_elements_through_a_simple_queue_again_in_the_same_process_without_autoAck()
+        {
+            var queueName = "amqp-conn-it-spec-simple-queue-" + Environment.TickCount;
+            var queueDeclaration = QueueDeclaration.Create(queueName);
+            var amqpSink = AmqpSink.CreateSimple(AmqpSinkSettings.Create(_connectionSettings).WithRoutingKey(queueName).WithDeclarations(queueDeclaration));
+
+            //#create-source-withoutautoack
+            var amqpSource = AmqpSource.CommittableSource(
+                NamedQueueSourceSettings.Create(_connectionSettings, queueName).WithDeclarations(queueDeclaration),
+                bufferSize: 10);
+            
+            //#create-source-withoutautoack
+
+            var input = new[] {"one", "two", "three", "four", "five"};
+            Source.From(input).Select(ByteString.FromString).RunWith(amqpSink, _mat).Wait();
+
+            //#run-source-withoutautoack
+            var result = amqpSource
+                .SelectAsync(1, async cm =>
+                {
+                    await cm.Ack().Task;
+                    return cm;
+                })
+                .Take(input.Length)
+                .RunWith(Sink.Seq<CommittableIncomingMessage>(), _mat);
+            
+            //#run-source-withoutautoack
+            result.Result.Select(x => x.Message.Bytes.ToString()).Should().Equal(input);
+        }
+
+        [Fact]
+        public void Republish_message_without_autoAck_if_nack_is_sent()
+        {
+            var queueName = "amqp-conn-it-spec-simple-queue-" + Environment.TickCount;
+            var queueDeclaration = QueueDeclaration.Create(queueName);
+            var amqpSink = AmqpSink.CreateSimple(AmqpSinkSettings.Create(_connectionSettings).WithRoutingKey(queueName).WithDeclarations(queueDeclaration));
+
+            var input = new[] {"one", "two", "three", "four", "five"};
+            Source.From(input).Select(ByteString.FromString).RunWith(amqpSink, _mat).Wait();
+
+            var amqpSource = AmqpSource.CommittableSource(
+                NamedQueueSourceSettings.Create(_connectionSettings, queueName).WithDeclarations(queueDeclaration), bufferSize: 10);
+
+            //#run-source-withoutautoack-and-nack
+            var result1 = amqpSource
+                .Take(input.Length)
+                .SelectAsync(1, async cm =>
+                {
+                    await cm.Nack().Task;
+                    return cm;
+                })
+                .RunWith(Sink.Seq<CommittableIncomingMessage>(), _mat);
+            
+            //#run-source-withoutautoack-and-nack
+            result1.Wait(TimeSpan.FromSeconds(3)).Should().BeTrue();
+
+            var result2 = amqpSource
+                .SelectAsync(1, async cm =>
+                {
+                    await cm.Ack().Task;
+                    return cm;
+                })
+                .Take(input.Length)
+                .RunWith(Sink.Seq<CommittableIncomingMessage>(), _mat);
+
+            result2.Result.Select(x => x.Message.Bytes.ToString()).Should().Equal(input);
+        }
+
+        [Fact]
+        public void Keep_connection_open_if_downstream_closes_and_there_are_pending_acks()
+        {
+            var queueName = "amqp-conn-it-spec-simple-queue-" + Environment.TickCount;
+            var queueDeclaration = QueueDeclaration.Create(queueName);
+
+            var amqpSink = AmqpSink.CreateSimple(AmqpSinkSettings.Create(_connectionSettings).WithRoutingKey(queueName).WithDeclarations(queueDeclaration));
+            
+            var amqpSource = AmqpSource.CommittableSource(
+                NamedQueueSourceSettings.Create(_connectionSettings, queueName).WithDeclarations(queueDeclaration), bufferSize: 10);
+
+            var input = new[] {"one", "two", "three", "four", "five"};
+            Source.From(input).Select(ByteString.FromString).RunWith(amqpSink, _mat).Wait();
+            var result = amqpSource.Take(input.Length).RunWith(Sink.Seq<CommittableIncomingMessage>(), _mat);
+
+            var _ = result.Result.Select(cm => cm.Ack().Task.Result.Should().Be(Done.Instance)).ToArray();
+        }
+
+        [Fact]
+        public void Not_republish_message_without_autoAck_false_if_nack_is_sent()
+        {
+            var queueName = "amqp-conn-it-spec-simple-queue-" + Environment.TickCount;
+            var queueDeclaration = QueueDeclaration.Create(queueName);
+            var amqpSink = AmqpSink.CreateSimple(AmqpSinkSettings.Create(_connectionSettings).WithRoutingKey(queueName).WithDeclarations(queueDeclaration));
+            var input = new[] {"one", "two", "three", "four", "five"};
+            Source.From(input).Select(ByteString.FromString).RunWith(amqpSink, _mat).Wait();
+
+            var amqpSource = AmqpSource.CommittableSource(
+                NamedQueueSourceSettings.Create(_connectionSettings, queueName).WithDeclarations(queueDeclaration), bufferSize: 10);
+
+            var result1 = amqpSource
+                .SelectAsync(1, async cm =>
+                {
+                    await cm.Nack(requeue: false).Task;
+                    return cm;
+                })
+                .Take(input.Length)
+                .RunWith(Sink.Seq<CommittableIncomingMessage>(), _mat);
+
+            result1.Wait(TimeSpan.FromSeconds(3)).Should().BeTrue();
+
+            var result2 = amqpSource
+                .SelectAsync(1, async cm =>
+                {
+                    await cm.Ack().Task;
+                    return cm;
+                })
+                .Take(input.Length)
+                .RunWith(Sink.Seq<CommittableIncomingMessage>(), _mat);
+
+            result2.Wait(TimeSpan.FromSeconds(1)).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Publish_via_RPC_and_then_consume_through_a_simple_queue_again_in_the_same_process_without_autoAck()
+        {
+            var queueName = "amqp-conn-it-spec-rpc-queue-" + Environment.TickCount;
+            var queueDeclaration = QueueDeclaration.Create(queueName);
+
+            var input = new[] {"one", "two", "three", "four", "five"};
+
+            var amqpRpcFlow = AmqpRpcFlow.CommittableFlow(
+                AmqpSinkSettings.Create(_connectionSettings).WithRoutingKey(queueName).WithDeclarations(queueDeclaration), bufferSize: 10);
+
+            var t =
+                Source.From(input)
+                    .Select(ByteString.FromString)
+                    .Select(bytes => new OutgoingMessage(bytes, false, false))
+                    .ViaMaterialized(amqpRpcFlow, Keep.Right)
+                    .SelectAsync(1, async cm =>
+                    {
+                        await cm.Ack().Task;
+                        return cm.Message;
+                    })
+                    .ToMaterialized(this.SinkProbe<IncomingMessage>(), Keep.Both)
+                    .Run(_mat);
+            
+            var rpcQueueF = t.Item1;
+            var probe = t.Item2;
+            rpcQueueF.Wait();
+
+            var amqpSink = AmqpSink.ReplyTo(AmqpReplyToSinkSettings.Create(_connectionSettings));
+
+            var amqpSource = AmqpSource.AtMostOnceSource(NamedQueueSourceSettings.Create(_connectionSettings, queueName), bufferSize: 1);
+            
+            amqpSource
+                .Select(b => new OutgoingMessage(b.Bytes, false, false, b.Properties))
+                .RunWith(amqpSink, _mat);
+
+            probe.ToStrict(TimeSpan.FromSeconds(3)).Select(x => x.Bytes.ToString()).Should().Equal(input);
+        }
+
+        [Fact]
+        public void Set_routing_key_per_message_and_consume_them_in_the_same_process()
+        {
+            string GetRoutingKey(string s) => $"key.{s}";
+
+            var exchangeName = "amqp.topic." + Environment.TickCount;
+            var queueName = "amqp-conn-it-spec-simple-queue-" + Environment.TickCount;
+            var exchangeDeclaration = ExchangeDeclaration.Create(exchangeName, "topic");
+            var queueDeclaration = QueueDeclaration.Create(queueName);
+            var bindingDeclaration = BindingDeclaration.Create(queueName, exchangeName).WithRoutingKey(GetRoutingKey("*"));
+
+            var amqpSink = AmqpSink.Create(
+                AmqpSinkSettings.Create(_connectionSettings)
+                    .WithExchange(exchangeName)
+                    .WithDeclarations(exchangeDeclaration, queueDeclaration, bindingDeclaration));
+
+            var amqpSource = AmqpSource.AtMostOnceSource(
+                NamedQueueSourceSettings.Create(_connectionSettings, queueName).WithDeclarations(exchangeDeclaration, queueDeclaration, bindingDeclaration),
+                bufferSize: 10);
+
+            var input = new[] {"one", "two", "three", "four", "five"};
+            var routingKeys = input.Select(GetRoutingKey);
+
+            Source.From(input)
+                .Select(s => new OutgoingMessage(ByteString.FromString(s), false, false, routingKey: GetRoutingKey(s)))
+                .RunWith(amqpSink, _mat)
+                .Wait();
+
+            var result = amqpSource
+                .Take(input.Length)
+                .RunWith(Sink.Seq<IncomingMessage>(), _mat)
+                .Result;
+
+            result.Select(x => x.Envelope.RoutingKey).Should().Equal(routingKeys);
+            result.Select(x => x.Bytes.ToString()).Should().Equal(input);
         }
         
         [Fact]

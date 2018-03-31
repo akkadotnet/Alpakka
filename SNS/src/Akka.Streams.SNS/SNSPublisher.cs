71d4dbcbf17d52f6aead9c4015a4d325ed88b39f
Sean Farrow <SeanFarrow@users.noreply.github.com>
2018-03-31 23:31:58 +0100

    Port the Alpakka sns sink (#11)
    * Add the skeleton projects for an AWS sns stream source/sink.
    
    * Update the NuGet pacakges in the source project.
    
    * Update the NuGet packages for the test project. Also, remove unused packages.
    
    * Add NSubstitute v2.3 to the tests project.
    
    * Finish adding the test checking that a single message is sent successfully.
    
    * Add a checkin the test checking that a single message is sent athat the PublishAsync method is called only once.
    
    * Add a test checking that multiple messages are published to sns.
    
    * Rename the test checking that only one message is published to sns.
    
    * Add a test checking that the stage fails with an exception if the PublishAsync method fails.
    
    * Add a test checking that the stage fails when an exception is thrown upstream.
    
    * Add a check that the PublishAsync method of the AmazonSnsClient is called once to the test checking that the stage is failed if the PublishAsync method throws an exception.
    
    * Remove unused namespaces from the spec.
    
    * Port the code for the sns publish flow stage.
    
    * Update the AWS SDK core package.
    
    * Add the equivalent of hte SNSPublishers sink method.
    
    * Update hte previously added methods name.
    
    * Move the SetHandler calls in to the constructor.
    
    * Move the flow shape creation to the PublishFlowStage constructor.
    
    * Update the AssemblyInfo.cs files of the sns and tests projects to reference SNS rather than SignalR.
    
    * Remove the readme for now.

---
+++ SNS/src/Akka.Streams.SNS/SNSPublisher.cs
@@ -0,0 +1,20 @@
+﻿using Akka.Streams.Dsl;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using System.Threading.Tasks;
+
+namespace Akka.Streams.SNS
+{
+    public static class SnsPublisher
+    {
+        public static Flow<string, PublishResponse, NotUsed> PublishToSNSFlow(string topicArn, IAmazonSimpleNotificationService snsService)
+        {
+            return Flow.FromGraph(new SNSPublishFlowStage(topicArn, snsService));
+        }
+
+        public static Sink<string, Task> PublishToSNSSink(string topicArn, IAmazonSimpleNotificationService snsService)
+        {
+            return PublishToSNSFlow(topicArn, snsService).ToMaterialized(Sink.Ignore<PublishResponse>(), Keep.Right);
+        }
+    }
+}

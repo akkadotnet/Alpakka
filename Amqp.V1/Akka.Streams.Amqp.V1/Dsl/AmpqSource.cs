653651f3c5f7127688e52bae0fd86a72408a3929
Unknown <andre.steenbergen@gmail.com>
2018-07-17 15:54:33 +0200

    Added support for Ampq v1.0

---
+++ Amqp.V1/Akka.Streams.Amqp.V1/Dsl/AmpqSource.cs
@@ -0,0 +1,12 @@
+﻿using Akka.Streams.Dsl;
+
+namespace Akka.Streams.Amqp.V1.Dsl
+{
+    public static class AmpqSource<T>
+    {
+        public static Source<T, NotUsed> Create(IAmqpSourceSettings<T> sourceSettings)
+        {
+            return Source.FromGraph(new AmqpSourceStage<T>(sourceSettings));
+        }
+    }
+}
---
+++ Amqp.V1/Akka.Streams.Amqp.V1/NamedQueueSinkSettings.cs
@@ -0,0 +1,34 @@
+﻿using Akka.IO;
+using Amqp;
+using Akka.Serialization;
+
+namespace Akka.Streams.Amqp.V1
+{
+
+    public class NamedQueueSinkSettings<T> : IAmpqSinkSettings<T>
+    {
+        private readonly Session session;
+        private readonly string linkName;
+        private readonly string queueName;
+        private readonly Serializer serializer;
+
+        public NamedQueueSinkSettings(
+            Session session,
+            string linkName,
+            string queueName,
+            Serializer serializer)
+        {
+            this.session = session;
+            this.linkName = linkName;
+            this.queueName = queueName;
+            this.serializer = serializer;
+        }
+
+        public byte[] GetBytes(T obj)
+        {
+            return serializer.ToBinary(obj);
+        }
+
+        public SenderLink GetSenderLink() => new SenderLink(session, linkName, queueName);
+    }
+}

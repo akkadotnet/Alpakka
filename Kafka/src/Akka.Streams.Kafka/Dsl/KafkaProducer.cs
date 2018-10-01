e6109808818eacb53978e7b6ef2337b1c45d1be0
Unknown <andre.steenbergen@gmail.com>
2018-10-01 09:04:58 +0200

    Added overloads to get Task for deliveryreport.
    It isn't always important to get deliveryreports. The async retrieval can make the flow slow, it needs to wait for the ACKs for the messages sent based on number of threads.

--- Kafka/src/Akka.Streams.Kafka/Dsl/KafkaProducer.cs
+++ Kafka/src/Akka.Streams.Kafka/Dsl/KafkaProducer.cs
@@ -18,7 +18,7 @@ namespace Akka.Streams.Kafka.Dsl
         /// </summary>
         public static Sink<MessageAndMeta<TKey, TValue>, Task> PlainSink<TKey, TValue>(ProducerSettings<TKey, TValue> settings)
         {
-            return PlainFlow(settings).ToMaterialized(Sink.Ignore<DeliveryReport<TKey, TValue>>(), Keep.Left);
+            return TaskFlow(settings).ToMaterialized(Sink.Ignore<Task<DeliveryReport<TKey, TValue>>>(), Keep.Left);
         }
 
         /// <summary>
@@ -26,7 +26,7 @@ namespace Akka.Streams.Kafka.Dsl
         /// </summary>
         public static Sink<MessageAndMeta<TKey, TValue>, Task> PlainSink<TKey, TValue>(ProducerSettings<TKey, TValue> settings, IProducer<TKey, TValue> producer)
         {
-            return PlainFlow(settings, producer).ToMaterialized(Sink.Ignore<DeliveryReport<TKey, TValue>>(), Keep.Left);
+            return TaskFlow(settings, producer).ToMaterialized(Sink.Ignore<Task<DeliveryReport<TKey, TValue>>>(), Keep.Left);
         }
 
         /// <summary>
@@ -46,6 +46,24 @@ namespace Akka.Streams.Kafka.Dsl
                 : flow.WithAttributes(ActorAttributes.CreateDispatcher(settings.DispatcherId));
         }
 
+
+        /// <summary>
+        /// Publish records to Kafka topics and then continue the flow. Possibility to pass through a message, which
+        /// can for example be a <see cref="CommittedOffsets"/> that can be committed later in the flow.
+        /// </summary>
+        public static Flow<MessageAndMeta<TKey, TValue>, Task<DeliveryReport<TKey, TValue>>, Task> TaskFlow<TKey, TValue>(ProducerSettings<TKey, TValue> settings)
+        {
+            var flow = Flow.FromGraph(new ProducerStage<TKey, TValue>(
+                    settings,
+                    closeProducerOnStop: true,
+                    producerProvider: settings.CreateKafkaProducer))
+                .Select(x => x);
+
+            return string.IsNullOrEmpty(settings.DispatcherId)
+                ? flow
+                : flow.WithAttributes(ActorAttributes.CreateDispatcher(settings.DispatcherId));
+        }
+
         /// <summary>
         /// Publish records to Kafka topics and then continue the flow. Possibility to pass through a message, which
         /// can for example be a <see cref="CommitableOffset"/> that can be committed later in the flow.
@@ -57,6 +75,23 @@ namespace Akka.Streams.Kafka.Dsl
                     closeProducerOnStop: false,
                     producerProvider: () => producer))
                 .SelectAsync(settings.Parallelism, x => x);
+
+            return string.IsNullOrEmpty(settings.DispatcherId)
+                ? flow
+                : flow.WithAttributes(ActorAttributes.CreateDispatcher(settings.DispatcherId));
+        }
+
+        /// <summary>
+        /// Publish records to Kafka topics and then continue the flow. Possibility to pass through a message, which
+        /// can for example be a <see cref="CommitableOffset"/> that can be committed later in the flow.
+        /// </summary>
+        public static Flow<MessageAndMeta<TKey, TValue>, Task<DeliveryReport<TKey, TValue>>, Task> TaskFlow<TKey, TValue>(ProducerSettings<TKey, TValue> settings, IProducer<TKey, TValue> producer)
+        {
+            var flow = Flow.FromGraph(new ProducerStage<TKey, TValue>(
+                    settings,
+                    closeProducerOnStop: false,
+                    producerProvider: () => producer))
+                .Select(x => x);
 
             return string.IsNullOrEmpty(settings.DispatcherId)
                 ? flow

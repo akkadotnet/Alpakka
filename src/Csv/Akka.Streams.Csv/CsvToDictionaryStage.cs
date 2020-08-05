62408204d4a74741c6f5c51a427d0f24ae3b9420
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-08-05 23:34:26 +0700

    Modernize Alpakka project structure (#65)
    * Use single solution file, reorganize file locations, use common.props
    
    * Reorganize file locations, use common.props
    
    * Update build system
    
    * Remove Kafka

---
+++ src/Csv/Akka.Streams.Csv/CsvToDictionaryStage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+using Akka.Streams.Stage;
+
+namespace Akka.Streams.Csv
+{
+    /// <summary>
+    /// Internal API: Converts incoming <see cref="ImmutableList{ByteString}"/> to <see cref="Dictionary{string, ByteString}"/>
+    /// </summary>
+    internal class CsvToDictionaryStage:GraphStage<FlowShape<ImmutableList<ByteString>, Dictionary<string, ByteString>>>
+    {
+        #region Logic
+        private sealed class Logic : InAndOutGraphStageLogic
+        {
+            private readonly CsvToDictionaryStage _stage;
+            private ImmutableList<string> _headers;
+            private readonly Encoding _encoding;
+
+            public Logic(CsvToDictionaryStage stage, ImmutableList<string> columnNames, Encoding encoding) : base(stage.Shape)
+            {
+                _stage = stage;
+                _headers = columnNames;
+                _encoding = encoding;
+
+                SetHandler(stage.In, this);
+                SetHandler(stage.Out, this);
+            }
+
+            public override void OnPush()
+            {
+                var elements = Grab(_stage.In);
+                if (_headers != null)
+                {
+                    var map = new Dictionary<string, ByteString>();
+                    var len = Math.Min(_headers.Count, elements.Count);
+                    for(int i = 0; i < len; ++i)
+                    {
+                        map.Add(_headers[i], elements[i]);
+                    }
+                    Push(_stage.Out, map);
+                }
+                else
+                {
+                    var headers = new List<string>();
+                    foreach (var elem in elements)
+                    {
+                        headers.Add(elem.ToString(_encoding));
+                    }
+                    _headers = headers.ToImmutableList();
+                    Pull(_stage.In);
+                }
+            }
+
+            public override void OnPull()
+            {
+                Pull(_stage.In);
+            }
+
+        }
+        #endregion
+
+        private readonly ImmutableList<string> _columnNames;
+        private readonly Encoding _encoding;
+
+        /// <summary>
+        /// Internal API: Converts incoming <see cref="ImmutableList{ByteString}"/> to <see cref="Dictionary{string, ByteString}"/>
+        /// </summary>
+        /// <param name="columnNames">If given, these names are used as map keys; if not first stream element is used</param>
+        /// <param name="encoding">Character encoding used to convert header line ByteString to String</param>
+        public CsvToDictionaryStage(ImmutableList<string> columnNames, Encoding encoding)
+        {
+            _columnNames = columnNames;
+            _encoding = encoding;
+
+            Shape = new FlowShape<ImmutableList<ByteString>, Dictionary<string, ByteString>>(In, Out);
+        }
+
+        protected override Attributes InitialAttributes { get; } = Attributes.CreateName("CsvToDictionary");
+
+        internal Inlet<ImmutableList<ByteString>> In { get; } = new Inlet<ImmutableList<ByteString>>("CsvToDictionary.in");
+        internal Outlet<Dictionary<string, ByteString>> Out { get; } = new Outlet<Dictionary<string, ByteString>>("CsvToDictionary.out");
+
+        public override FlowShape<ImmutableList<ByteString>, Dictionary<string, ByteString>> Shape { get; }
+
+        protected override GraphStageLogic CreateLogic(Attributes inheritedAttributes)
+            => new Logic(this, _columnNames, _encoding);
+
+        public override string ToString() => nameof(CsvToDictionaryStage);
+    }
+}
---
+++ src/Csv/Akka.Streams.Csv/Dsl/ByteOrderMark.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+
+namespace Akka.Streams.Csv.Dsl
+{
+    /// <summary>
+    /// Byte Order Marks may be used to indicate the used character encoding in text files.
+    /// @see http://www.unicode.org/faq/utf_bom.html#bom1
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "UTF, BE, and LE are acronyms")]
+    public class ByteOrderMark
+    {
+        /// <summary>
+        /// Byte Order Mark for UTF-16 big-endian
+        /// </summary>
+        public static readonly ByteString UTF16_BE;
+
+        /// <summary>
+        /// Byte Order Mark for UTF-16 little-endian
+        /// </summary>
+        public static readonly ByteString UTF16_LE;
+
+        /// <summary>
+        /// Byte Order Mark for UTF-32 big-endian
+        /// </summary>
+        public static readonly ByteString UTF32_BE;
+
+        /// <summary>
+        /// Byte Order Mark for UTF-32 little-endian
+        /// </summary>
+        public static readonly ByteString UTF32_LE;
+
+        /// <summary>
+        /// Byte Order Mark for UTF-8
+        /// </summary>
+        public static readonly ByteString UTF8;
+
+        public static readonly ByteString None;
+
+        static ByteOrderMark()
+        {
+            var ZeroZero = ByteString.FromBytes(new byte[]{0x00, 0x00});
+            UTF16_BE = ByteString.FromBytes(new byte[] { 0xfe, 0xff });
+            UTF16_LE = ByteString.FromBytes(new byte[] { 0xff, 0xfe });
+            UTF32_BE = ZeroZero + UTF16_BE;
+            UTF32_LE = UTF16_LE + ZeroZero;
+            UTF8 = ByteString.FromBytes(new byte[]{0xEF, 0xBB, 0xBF});
+            None = ByteString.Empty;
+        }
+    }
+}

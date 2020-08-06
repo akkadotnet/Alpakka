7168836acb4bc202cf1f90fa24bfa9e2e2ceff6a
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-08-06 21:14:28 +0700

    Test for another private azure emulator image.

--- src/Azure/Akka.Streams.Azure.StorageQueue.Tests/AzureFixture.cs
+++ src/Azure/Akka.Streams.Azure.StorageQueue.Tests/AzureFixture.cs
@@ -59,7 +59,7 @@ namespace Akka.Streams.Azure.StorageQueue.Tests
                     case OperatingSystem.Linux:
                         return "mcr.microsoft.com/azure-storage/azurite";
                     case OperatingSystem.Windows:
-                        return "arkatufus/azure-storage-emulator";
+                        return "farmer1992/azure-storage-emulator";
                     default:
                         throw new NotSupportedException($"Unsupported OS [{RuntimeInformation.OSDescription}]");
                 }

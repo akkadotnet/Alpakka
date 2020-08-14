2172aa574486edd298948e646312250eb448f2a0
Aaron Stannard <aaron@petabridge.com>
2020-08-14 14:49:12 -0500

    move all docker images into 'akkadotnet' Docker organization

--- support/dockerfiles/AMQP/Linux/build.ps1
+++ support/dockerfiles/AMQP/Linux/build.ps1
@@ -1,2 +1,2 @@
-docker build -t arkatufus/rabbitmq-linux:3.8.6-alpine .
-docker tag arkatufus/rabbitmq-linux:3.8.6-alpine arkatufus/rabbitmq-linux:latest
+docker build -t akkadotnet/rabbitmq-linux:3.8.6-alpine .
+docker tag akkadotnet/rabbitmq-linux:3.8.6-alpine arkatufus/rabbitmq-linux:latest
--- support/dockerfiles/Azure/Windows/build.ps1
+++ support/dockerfiles/Azure/Windows/build.ps1
@@ -1,2 +1,2 @@
-docker build -t arkatufus/azure-storage-emulator:ltsc2019 .
-docker tag arkatufus/azure-storage-emulator:ltsc2019 arkatufus/azure-storage-emulator:latest
+docker build -t akkadotnet/azure-storage-emulator:ltsc2019 .
+docker tag akkadotnet/azure-storage-emulator:ltsc2019 arkatufus/azure-storage-emulator:latest

dcd92ec39ddf872fc0449623c5cb3cad5cf0d11b
Gregorius Soedharmo <arkatufus@yahoo.com>
2020-12-18 02:47:47 +0700

    Add AMQP retry mechanism and example projects

---
+++ src/Amqp/Akka.Streams.Amqp.V1/AddressSinkSettings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Serialization;
+using Amqp;
+using Amqp.Framing;
+using Amqp.Types;
+
+namespace Akka.Streams.Amqp.V1
+{
+    public class AddressSinkSettings<T> : IAmqpSinkSettings<T>
+    {
+        private readonly string _linkName;
+        private readonly string _queueName;
+        private readonly Serializer _serializer;
+        private readonly Address _address;
+
+        private Connection _connection;
+        private Session _session;
+
+        public AddressSinkSettings(
+            Address address,
+            string linkName,
+            string queueName,
+            Serializer serializer)
+        {
+            _address = address;
+            _linkName = linkName;
+            _queueName = queueName;
+            _serializer = serializer;
+        }
+
+        public byte[] GetBytes(T obj)
+        {
+            return _serializer.ToBinary(obj);
+        }
+
+        public SenderLink GetSenderLink()
+        {
+            if (_connection == null || _connection.IsClosed)
+                _connection = new Connection(_address);
+
+            if (_session == null || _session.IsClosed)
+                _session = new Session(_connection);
+
+            return new SenderLink(
+                _session, 
+                _linkName,
+                new Target
+                {
+                    Address = _queueName, 
+                    Capabilities = new[] { new Symbol("queue") }
+                }, 
+                null);
+        }
+    }
+}
---
+++ src/Amqp/Examples/Amqp.V1.Sink/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Streams;
+using Akka.Streams.Amqp.V1;
+using Akka.Streams.Amqp.V1.Dsl;
+using Akka.Streams.Dsl;
+
+namespace Amqp.V1.Sink
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sys = ActorSystem.Create("AMQP-System-Sink");
+            var materializer = ActorMaterializer.Create(sys);
+            var serialization = sys.Serialization;
+            var serializer = serialization.FindSerializerForType(typeof(string));
+
+            var address = new Address("127.0.0.1", 5672, "guest", "guest", scheme: "AMQP");
+
+            var queueName = "akka.test";
+            var senderLinkName = "amqp-conn-test-sender";
+
+            var amqpSink = RestartSink.WithBackoff(() =>
+                {
+                    Console.WriteLine("Start/Restart...");
+                    return AmqpSink.Create(
+                        new AddressSinkSettings<string>(address, senderLinkName, queueName, serializer));
+                },
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(3),
+                0.2,
+                short.MaxValue
+            );
+
+            //run sink
+            var input = new[] { "one", "two", "three", "four", "five" };
+            Source
+                .Cycle(() => input.AsEnumerable().GetEnumerator())
+                .Delay(TimeSpan.FromSeconds(1))
+                .Select(s =>
+                {
+                    Console.WriteLine(s);
+                    return s;
+                })
+                .RunWith(amqpSink, materializer);
+
+            Console.ReadKey();
+
+            await sys.Terminate();
+        }
+    }
+}

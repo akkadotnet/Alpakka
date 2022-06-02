1b2a249b2a0ac34893c01ff0ed99d69c7ea45b84
Ismael Hamed <1279846+ismaelhamed@users.noreply.github.com>
2022-06-02 20:20:24 +0200

    Implemented new file module `DirectoryChangesSource` (#962)
    Co-authored-by: Aaron Stannard <aaron@petabridge.com>

---
+++ src/File/Akka.Streams.File/DirectoryChangesSource.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DirectoryChangesSource.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Akka.Streams.Dsl;
+using Akka.Streams.File.Impl;
+
+namespace Akka.Streams.File
+{
+    public static class DirectoryChangesSource
+    {
+        private static readonly Func<string, DirectoryChange, (string, DirectoryChange)> _tupler = (t, u) => (t, u);
+
+        /// <summary>
+        /// Watch directory and emit changes as a stream of tuples containing the path and type of change.
+        /// </summary>
+        /// <param name="directoryPath">Directory to watch</param>
+        /// <param name="maxBufferSize">Maximum number of buffered directory changes before the stage fails</param>
+        public static Source<(string, DirectoryChange), NotUsed> Create(string directoryPath, int maxBufferSize) =>
+            Source.FromGraph(new DirectoryChangesSource<(string, DirectoryChange)>(directoryPath, maxBufferSize, _tupler));
+    }
+}
---
+++ src/File/Akka.Streams.File/Utils/QueueExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Collections.Generic
+{
+    internal static class QueueExtensions
+    {
+        public static bool TryDequeue<T>(this Queue<T> queue, out T result)
+        {
+            if (queue.Count == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            result = queue.Dequeue();
+            return true;
+        }
+    }
+}

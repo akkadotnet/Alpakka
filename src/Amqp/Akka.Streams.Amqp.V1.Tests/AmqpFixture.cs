17622a05ab2cb662a88d736b682b63ed2dd475d2
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-03-29 01:19:05 +0700

    Change Amqp to use TestContainers

--- src/Amqp/Akka.Streams.Amqp.V1.Tests/AmqpFixture.cs
+++ src/Amqp/Akka.Streams.Amqp.V1.Tests/AmqpFixture.cs
@@ -1,159 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
-using Akka.Util;
-using Docker.DotNet;
-using Docker.DotNet.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using Akka.Streams.Amqp.V1.Tests;
+using Testcontainers.RabbitMq;
 using Xunit;
-using Xunit.Abstractions;
-using Address = Amqp.Address;
 
-namespace Akka.Streams.Amqp.Tests
+#nullable enable
+namespace Akka.Streams.Amqp.Tests;
+
+[CollectionDefinition("AmqpSpec")]
+public sealed class AmqpSpecFixture : ICollectionFixture<AmqpFixture>
 {
-    [CollectionDefinition("AmqpSpec")]
-    public sealed class AmqpSpecFixture : ICollectionFixture<AmqpFixture>
+}
+
+public class AmqpFixture : IAsyncLifetime
+{
+    protected readonly string RabbitContainerName = $"rabbit-{Guid.NewGuid():N}";
+    public string? ConnectionString { get; private set; }
+    public string HostName => Container!.Hostname;
+    public int AmqpPort => Container!.GetMappedPublicPort(RabbitMqBuilder.RabbitMqPort);
+
+    public string UserName => "guest";
+    public string Password => "guest";
+
+    public RabbitMqContainer? Container { get; protected set; }
+
+    public async Task InitializeAsync()
     {
+        using var outputConsumer = new OutputConsumer();
+        
+        Container = new RabbitMqBuilder()
+            .WithImage("akkadotnet/rabbitmq-linux")
+            .WithName(RabbitContainerName)
+            .WithUsername(UserName)
+            .WithPassword(Password)
+            .WithOutputConsumer(outputConsumer)
+            .Build();
+
+        await Container.StartAsync();
+        
+        await outputConsumer.WaitUntilReadyAsync("Server startup complete", 1, TimeSpan.FromMinutes(1));
+
+        ConnectionString = Container.GetConnectionString();
     }
 
-    public enum OperatingSystem
+    public async Task DisposeAsync()
     {
-        NotInitialized,
-        Windows,
-        Linux,
-        Unknown
-    }
-
-    public class AmqpFixture : IAsyncLifetime
-    {
-        protected readonly string AqmpContainerName = $"amqp-{Guid.NewGuid():N}";
-        protected DockerClient Client;
-
-        protected string ImageName => "akkadotnet/rabbitmq-linux"; 
-        protected string ImageTag => "latest";
-        protected string AmqpImageName => $"{ImageName}:{ImageTag}";
-
-        public string UserName { get; set; } = "guest";
-        public string Password { get; set; } = "guest";
-        public string HostName { get; set; } = "127.0.0.1";
-
-        public int AmqpPort => 5672;
-
-        public int AmqpsPort => 5671;
-
-        public int EpmdPort => 4369;
-
-        public async Task InitializeAsync()
+        if (Container != null)
         {
-            Client = new DockerClientConfiguration().CreateClient();
-
-            var images = await Client.Images.ListImagesAsync(new ImagesListParameters
-            {
-                Filters = new Dictionary<string, IDictionary<string, bool>>()
-                {
-                    ["reference"] = new Dictionary<string, bool>()
-                    {
-                        [AmqpImageName] = true
-                    }
-                }
-            });
-            if (images.Count == 0)
-                await Client.Images.CreateImageAsync(
-                    new ImagesCreateParameters { FromImage = ImageName, Tag = ImageTag }, null,
-                    new Progress<JSONMessage>(message =>
-                    {
-                        Console.WriteLine(!string.IsNullOrEmpty(message.ErrorMessage)
-                            ? message.ErrorMessage
-                            : $"{message.ID} {message.Status} {message.ProgressMessage}");
-                    }));
-
-            var exposedPorts = new Dictionary<string, EmptyStruct>
-            {
-                { "4369/tcp", new EmptyStruct() },
-                { "5671/tcp", new EmptyStruct() },
-                { "5672/tcp", new EmptyStruct() },
-            };
-
-            var portBindings = new Dictionary<string, IList<PortBinding>>
-            {
-                { "4369/tcp", new List<PortBinding> { new PortBinding { HostPort = $"{EpmdPort}" } } },
-                { "5671/tcp", new List<PortBinding> { new PortBinding { HostPort = $"{AmqpsPort}" } } },
-                { "5672/tcp", new List<PortBinding> { new PortBinding { HostPort = $"{AmqpPort}" } } },
-            };
-
-            // create the container
-            await Client.Containers.CreateContainerAsync(new CreateContainerParameters
-            {
-                Image = $"{ImageName}:{ImageTag}",
-                Name = AqmpContainerName,
-                Tty = true,
-                ExposedPorts = exposedPorts,
-                HostConfig = new HostConfig { PortBindings = portBindings }
-            });
-
-            // start the container
-            await Client.Containers.StartContainerAsync(AqmpContainerName, new ContainerStartParameters());
-
-            // Ping server continuously until either we timed out or the server is up.
-            // RabbitMQ Windows image takes a very long time to spin up!
-            var gracePeriod = TimeSpan.FromMinutes(2);
-            var tcpClient = new TcpClient();
-            var stopWatch = Stopwatch.StartNew();
-            try
-            {
-                while (stopWatch.Elapsed < gracePeriod)
-                {
-                    // Ping the server every second
-                    await Task.Delay(TimeSpan.FromSeconds(1));
-                    try
-                    {
-                        await tcpClient.ConnectAsync(HostName, AmqpPort);
-                        tcpClient.Close();
-                        break;
-                    }
-                    catch (SocketException)
-                    {
-                        // no-op
-                    }
-                }
-                stopWatch.Stop();
-
-                if (stopWatch.Elapsed >= gracePeriod)
-                    throw new Exception($"RabbitMQ server did not respond within grace period of {gracePeriod.TotalSeconds} seconds.");
-            }
-            finally
-            {
-                tcpClient.Dispose();
-            }
-
-            // Provide a 60 second startup delay
-            // await Task.Delay(TimeSpan.FromSeconds(60));
-        }
-
-        public async Task<bool> StopContainer()
-            => await Client.Containers.StopContainerAsync(AqmpContainerName, new ContainerStopParameters());
-
-        public async Task<bool> StartContainer()
-            => await Client.Containers.StartContainerAsync(AqmpContainerName, new ContainerStartParameters());
-
-        public async Task DisposeAsync()
-        {
-            if (Client != null)
-            {
-                await Client.Containers.StopContainerAsync(AqmpContainerName, new ContainerStopParameters());
-                await Client.Containers.RemoveContainerAsync(AqmpContainerName,
-                    new ContainerRemoveParameters { Force = true });
-                Client.Dispose();
-            }
+            await Container.StopAsync();
+            await Container.DisposeAsync();
         }
     }
 }

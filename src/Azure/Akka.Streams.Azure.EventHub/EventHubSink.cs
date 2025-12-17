83f1c217d32e58e164097f8221c1f4ba644ce61e
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-12-17 12:18:49 +0700

    Enable Incrementalist.Cmd in CI/CD

--- src/Azure/Akka.Streams.Azure.EventHub/EventHubSink.cs
+++ src/Azure/Akka.Streams.Azure.EventHub/EventHubSink.cs
@@ -19,6 +19,8 @@ using Directive = Akka.Streams.Supervision.Directive;
 
 namespace Akka.Streams.Azure.EventHub
 {
+    // incrementalist test
+    
     /// <summary>
     /// A <see cref="Sink{TIn,TMat}"/> for the Azure EventHub
     /// </summary>
